d6181eaac22ca69d2b055fa4e796b3ad13b1d5f9
LPRowe <48843359+LPRowe@users.noreply.github.com>
2020-10-23 02:20:36 -0500

    added bird radar and crit radar imagery all gui is functional next step is to check the bird radar visual to see if theta is handled correctly

--- boid_simulation.py
+++ boid_simulation.py
@@ -9,12 +9,72 @@ import pygame
 import time
 import glob
 import math, random
-from boid_tools import Boid, Boid_Cloud
+from boid_tools import Boid, Boid_Cloud, GUI
 
-def main():
+
+
+
+
+
+
+
+
+
+class Click_Action(object):
+    def __init__(self, action_map):
+        self.click_map = {(34, 22, 78, 80):'phi_up',
+                          (38, 165, 72, 217):'phi_down',
+                          (354, 24, 392, 63):'r_down',
+                          (693, 24, 728, 61):'r_up',
+                          (361, 99, 394, 138):'rc_down',
+                          (692, 100, 725, 137):'rc_up',
+                          (361, 182, 395, 214):'max_turn_down',
+                          (691, 178, 726, 215):'max_turn_up',
+                          (897, 29, 949, 84):'speed_up',
+                          (897, 164, 948, 224):'speed_down',
+                          (1225, 27, 1272, 85):'num_birds_up',
+                          (1221, 166, 1274, 223):'num_birds_down',
+                          (1781, 23, 1821, 58):'separation_up',
+                          (1394, 27, 1430, 60):'separation_down',
+                          (1784, 98, 1820, 135):'alignment_up',
+                          (1394, 101, 1430, 137):'alignment_down',
+                          (1783, 179, 1816, 214):'cohesion_up',
+                          (1393, 179, 1429, 215):'cohesion_down',
+                          (1835, 89, 1982, 154):'reset',
+                          (1836, 163, 1986, 232):'start'
+                          }
+        
+        self.action_map = {'phi_up': 1,
+                          'phi_down': -1,
+                          'r_down': -5,
+                          'r_up': 5,
+                          'rc_down': -0.01,
+                          'rc_up': 0.01,
+                          'max_turn_down': -1,
+                          'max_turn_up': 1,
+                          'speed_up': 1,
+                          'speed_down': -1,
+                          'num_birds_up': None,
+                          'num_birds_down': None,
+                          'separation_up': 0.01,
+                          'separation_down': -0.01,
+                          'alignment_up': 0.01,
+                          'alignment_down': -0.01,
+                          'cohesion_up': 0.01,
+                          'cohesion_down': -0.01,
+                          'reset': None,
+                          'start': None
+                          }
+    
+    def _click_to_action(self, x, y):
+        for x1, y1, x2, y2 in self.click_map:
+            if x1 <= x <= x2 and y1 <= y <= y2:
+                return self.click_map[(x1, y1, x2, y2)]
+
+
+def main(gui, bird_phi, bird_radius, crit_radius, max_turn, bird_speed, num_birds, separation_coef, alignment_coef, cohesion_coef, sleep_time):
     
     #Surface settings
-    window_width, window_height = 1500, 1000
     window_width, window_height = 2000, 1200
     header_body_ratio = 0.8
     bg_width, bg_height = window_width, window_height * header_body_ratio
@@ -27,38 +87,39 @@ def main():
     bird_images = []
     f = lambda x: (1 + math.sin(2*x)**2) / 4 + (3 / 4)
     variable_scale = True #have birds fluctuate in size based on orientation (looks like elevation change)
-    for theta in range(-135,360-135,2):
+    for theta in range(-135,360-135,2): # - 135 so 0 is along the x+ axis
         scale = 1 / f(theta * math.pi / 180) if variable_scale else 1
         w, h = int(bird_width * scale), int(bird_height * scale)
         img = pygame.transform.scale(bird_image, (w, h))
         img = pygame.transform.rotozoom(img, theta, 1)
         bird_images.append(img)
     
+    # Bird for radar symbol
+    radar_bird = bird_images[45].copy()
+    
     #Add GUI Header
     bg_board = [pygame.image.load('./graphics/gui_layout.png')]
     
     for b in bg_board:
-        pygame.transform.scale(b, (int(bg_width),int(bg_height)))
+        b = pygame.transform.scale(b, (int(gui_width),int(gui_height)))
     
-    #Limit game speed
-    sleep_time = 0
-    
-    # Set Bird properties
-    num_birds = 20
-    bird_speed = 11
-    bird_radius = 200
-    bird_phi = 85
-    
+    # Populate birds
     boids = []
     for _ in range(num_birds):
         theta = random.randint(0, 359)
-        #theta = 45
         x = random.randint(bird_width, bg_width - bird_width)
-        y = gui_height + bird_height + random.randint(0, bg_height-2*bird_height)
+        y = gui_height + bird_height + random.randint(0, bg_height - 2*bird_height)
         boids.append(Boid(x, y, theta, bird_speed, bird_radius, bird_phi, (0, gui_height, window_width-bird_width, window_height-bird_width)))
-        
+    
+    # Track all boids positions and velocities in the boid cloud
     cloud = Boid_Cloud(walls = (0, gui_height, window_width, window_height))
     
+    # Convert clicks on gui to actions
+    action_map = {'phi_up': bird_phi, 'phi_down': bird_phi,
+                  'r_up': bird_radius, 'r_down': bird_radius}
+    click = Click_Action(action_map)
+    
+    run = True
     
     while True:
         time.sleep(sleep_time) #slow down run speed for all algorithms besides A*
@@ -69,13 +130,82 @@ def main():
         
         keys=pygame.key.get_pressed()
         if keys[pygame.K_f]:
-            pass
+            sleep_time = max(0, sleep_time - 0.01)
+        elif keys[pygame.K_s]:
+            sleep_time = min(0.1, sleep_time + 0.01)
         
         #Handle Mouse Clicks for buttons
         mouse = pygame.mouse
-        if mouse.get_pressed()[0] and (mouse.get_pos()[1] >= gui_height):
+        if mouse.get_pressed()[0] and (mouse.get_pos()[1] <= gui_height):
             x,y = mouse.get_pos()
             print(x,y)
+            action = click._click_to_action(x,y)
+            if action:
+                if action == 'num_birds_up':
+                    theta = random.randint(0, 359)
+                    x = random.randint(bird_width, bg_width - bird_width)
+                    y = gui_height + bird_height + random.randint(0, bg_height-2*bird_height)
+                    boids.append(Boid(x, y, theta, bird_speed, bird_radius, bird_phi, (0, gui_height, window_width-bird_width, window_height-bird_width)))
+                    gui.num_birds = len(boids)
+                elif action == 'num_birds_down':
+                    if len(boids) > 1:
+                        boids.pop()
+                    gui.num_birds = len(boids)
+                elif action == 'reset':
+                    for bird in boids:
+                        x = random.randint(bird_width, bg_width - bird_width)
+                        y = gui_height + bird_height + random.randint(0, bg_height-2*bird_height)
+                        bird.x, bird.y = x, y
+                elif action == 'start':
+                    run = not run
+                elif 'phi' in action:
+                    bird_phi += 1 if 'up' in action else -1
+                    bird_phi = max(0, min(bird_phi, 180))
+                    gui.bird_phi = bird_phi
+                    gui.generate_radar_points()
+                    print('phi', bird_phi)
+                    for bird in boids: bird.phi = bird_phi
+                elif 'rc' in action:
+                    crit_radius += 0.01 if 'up' in action else -0.01
+                    crit_radius = max(0, min(crit_radius, 1))
+                    gui.crit_radius = crit_radius
+                    gui.generate_radar_points()
+                    for bird in boids: bird.crit_radius = crit_radius * bird.radius
+                elif 'turn' in action:
+                    max_turn += 0.25 if 'up' in action else -0.25
+                    max_turn = max(0, min(max_turn, 180))
+                    gui.max_turn = max_turn
+                    for bird in boids: bird.max_turn = max_turn
+                elif 'speed' in action:
+                    bird_speed += 0.5 if 'up' in action else -0.5
+                    bird_speed = max(0, min(bird_speed, 30))
+                    gui.bird_speed = bird_speed
+                    for bird in boids: bird.speed = bird_speed
+                elif 'r_' in action:
+                    bird_radius += 1 if 'up' in action else -1
+                    bird_radius = max(0, min(bird_radius, 2000))
+                    gui.bird_radius = bird_radius
+                    gui.generate_radar_points()
+                    for bird in boids: bird.radius = bird_radius
+                elif 'separation' in action:
+                    separation_coef += 0.01 if 'up' in action else -0.01
+                    separation_coef = max(0, min(separation_coef, 1))
+                    gui.separation_coef = separation_coef
+                    for bird in boids: bird.behavior['social_distancing'] = separation_coef
+                elif 'alignment' in action:
+                    alignment_coef += 0.01 if 'up' in action else -0.01
+                    alignment_coef = max(0, min(alignment_coef, 1))
+                    gui.alignment_coef = alignment_coef
+                    for boid in boids: bird.behavior['alignment'] = alignment_coef
+                elif 'cohesion' in action:
+                    cohesion_coef += 0.01 if 'up' in action else -0.01
+                    cohesion_coef = max(0, min(cohesion_coef, 1))
+                    gui.cohesion_coef = cohesion_coef
+                    for bird in boids: bird.behavior['center_of_mass'] = cohesion_coef
+
+                
+        elif mouse.get_pressed()[0]:
+            # scatter birds
             pass
         
         surface.fill((255, 255, 255))
@@ -83,14 +213,16 @@ def main():
         cloud.update(boids)
 
         for i,bird in enumerate(boids):
-            #bird.theta = (bird.theta + 1) % 360
             bird.update(*cloud.visible_birds(bird.x, bird.y, bird.theta, bird.radius, bird.phi, i, bird.crit_radius),
                         cloud.positions,
                         cloud.velocities,
-                        cloud.too_close_to_wall)
+                        cloud.too_close_to_wall,
+                        cloud.center_mass)
             bird.draw(surface, bird_images[int(bird.theta // 2) % len(bird_images)])
         
         surface.blit(bg_board[0], (0, 0))
+        gui.draw(surface)
+        surface.blit(radar_bird, (202, 97)) # draw bird at center of radar
         
         pygame.display.flip()
 
@@ -100,4 +232,17 @@ if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('Boid Simulation')
     
-    main()
+    settings = {
+                'bird_phi' : 85,       # how wide of an angle can birds see (0 .. 180) phi = 180 is 360 deg vision
+                'bird_radius' : 400,   # how far bird can see
+                'crit_radius' : 0.25,  # percent of visual radius 
+                'max_turn' : 3,        # maximum degrees a bird can turn in one frame
+                'bird_speed' : 10,     # pixels / second
+                'num_birds' : 100,
+                'separation_coef' : 1, # bird law coefficients
+                'alignment_coef' : 1,
+                'cohesion_coef' : 1, 
+                'sleep_time' : 0,      # limit play speed by pausing sleep_time [ms] between frames
+                }
+    
+    main(gui = GUI(**settings), **settings)
--- boid_tools.py
+++ boid_tools.py
@@ -8,6 +8,90 @@ Created on Tue Oct 20 22:08:16 2020
 import pygame
 import math
 import functools
+import numpy as np
+
+class GUI(object):
+    def __init__(self, bird_phi, bird_radius, crit_radius, max_turn, bird_speed, num_birds, separation_coef, alignment_coef, cohesion_coef, sleep_time):
+        
+        # Bird params
+        self.bird_phi = bird_phi
+        self.bird_radius = bird_radius
+        self.bird_speed = bird_speed
+        self.crit_radius = crit_radius
+        self.max_turn = max_turn
+        self.num_birds = num_birds
+        self.separation_coef = separation_coef
+        self.alignment_coef = alignment_coef
+        self.cohesion_coef = cohesion_coef
+        
+        # Font
+        self.font = pygame.font.SysFont('tahoma', 35, bold=True)
+        self.med_font = pygame.font.SysFont('tahoma', 26, bold=True)
+        self.small_font = pygame.font.SysFont('tahoma', 18, bold=True)
+        self.font_color = (255,255,255)
+        self.offset = 28
+        
+        # Bird Radar (only affects radar image on gui)
+        self.radar_center = (232, 124)                   # center of bird radar image
+        self.max_rad = 110                               # max radius of radar image [pixels]
+        self.theta_offset = -math.pi / 2                  # to help orient the radar vertically
+        self.generate_radar_points() # points for bird radar polygon
+        self.radar_color = (88, 214, 141)
+        self.crit_radar_color = (255, 124, 124)
+        
+    def generate_radar_points(self):
+        phi = self.bird_phi * math.pi / 180
+        radar_thetas = np.linspace(-phi + self.theta_offset, phi + self.theta_offset, 120)
+        radar_radius = self.max_rad if self.bird_radius >= 500 else self.max_rad * self.bird_radius / 500
+        xc, yc = self.radar_center
+        self.radar_points = [self.radar_center] + [(int(xc + radar_radius * math.cos(t)), 
+                                                    int(yc + radar_radius * math.sin(t))) for t in radar_thetas]
+        self.crit_radar_points = [self.radar_center] + [(int(xc + self.crit_radius * radar_radius * math.cos(t)), 
+                                                         int(yc + self.crit_radius * radar_radius * math.sin(t))) for t in radar_thetas]
+    
+    def draw(self, surface):
+        
+        # Radar Polygon
+        pygame.draw.polygon(surface, self.radar_color, self.radar_points)
+        
+        # Radar Crit Radius
+        pygame.draw.polygon(surface, self.crit_radar_color, self.crit_radar_points)
+
+        # Radar Bird
+        
+        # R
+        text_radius = self.font.render(str(int(self.bird_radius)) + " pixels", 1, self.font_color)
+        surface.blit(text_radius, (459,48-self.offset))
+        
+        # Rc
+        text_crit = self.font.render(str(int(100 * self.crit_radius)) + " %", 1, self.font_color)
+        surface.blit(text_crit, (480,125-self.offset))
+        
+        # Max Turn
+        text_turn = self.font.render(str(int(self.max_turn)), 1, self.font_color)
+        text_deg = self.small_font.render("o", 1, self.font_color)
+        surface.blit(text_turn, (598,201-self.offset))
+        surface.blit(text_deg, (598 + text_turn.get_width(), 201 - 1.2*self.offset))
+        
+        # num_birds
+        text_num_birds = self.font.render(str(self.num_birds), 1, self.font_color)
+        surface.blit(text_num_birds, (1254, 127 - self.offset))
+        
+        # Separation
+        text_sep = self.font.render(str(int(100 * self.separation_coef)) + str('%'), 1, self.font_color)
+        surface.blit(text_sep, (1667, 50 - self.offset))
+        
+        # Alignment
+        text_align = self.font.render(str(int(100 * self.alignment_coef)) + str('%'), 1, self.font_color)
+        surface.blit(text_align, (1651, 126 - self.offset))
+        
+        # Cohesion
+        text_cohesion = self.font.render(str(int(100 * self.cohesion_coef)) + str('%'), 1, self.font_color)
+        surface.blit(text_cohesion, (1620, 201 - self.offset))
+        
+        # Cohesion
+        text_speed = self.med_font.render(str(int(self.bird_speed)) + " pixels/frame", 1, self.font_color)
+        surface.blit(text_speed, (851, 125 - 0.5*self.offset))
 
 class Boid_Cloud(object):
     def __init__(self, walls = (0, 240, 2000, 1200)):
@@ -21,10 +105,16 @@ class Boid_Cloud(object):
         self.too_close_to_wall = set()
         self.x_min, self.y_min, self.x_max, self.y_max = walls
         self.crit_radius = 100
+        self.center_mass = [(self.x_min + self.x_max) // 2, (self.y_min + self.y_max) // 2] # center of mass of all the boids initialized as screen center
     
     def update(self, boid_list):
+        self.positions = []
+        self.velocities = []
+        self.center_mass = [0, 0]
         for i, bird in enumerate(boid_list):
             x, y = bird.x, bird.y
+            self.center_mass[0] += x
+            self.center_mass[1] += y
             self.positions.append((x,y))
             self.velocities.append(bird.get_vel())
             
@@ -67,19 +157,19 @@ class Boid(object):
         self.x = x
         self.y = y
         self.theta = theta
-        self.s = s # speed
+        self.speed = s
         
         # vision params
         self.radius = radius # can see all birds within self.radius pixel radius
         self.phi = phi # cannot see birds in the 60 degree window behind it
         
         # control params: what does the bird consider when updating it's movement
-        self.behavior = {'center_of_mass': 1,    # steer towards the center of mass of neighbors (weight 0 .. 1)
+        self.behavior = {'center_of_mass': .25,    # steer towards the center of mass of neighbors (weight 0 .. 1)
                          'social_distancing': 1, # don't get too close to neighbors (weight 0 .. 1)
-                         'alignment': 0}         # match neighbor's velocity vector (weight 0 .. 1)
+                         'alignment': 1}         # match neighbor's velocity vector (weight 0 .. 1)
         
         # turning and velocity change capabilities
-        self.max_turn = 6 # cannot change more than 10 degrees in 1 frame
+        self.max_turn = 3 # cannot change more than 10 degrees in 1 frame
         self.max_acceleration = 0.2 # cannot change speed by more than 20% in 1 frame
         
         # bounds boid should not cross
@@ -97,13 +187,13 @@ class Boid(object):
         '''
         #theta = (self.theta - 90) % 360 # because self.theta is wrt y- axis
         theta = self.theta
-        x_vel = self.s * math.cos(theta * math.pi / 180)
-        y_vel = -1 * self.s * math.sin(theta * math.pi / 180) # remember down is positive for y
+        x_vel = self.speed * math.cos(theta * math.pi / 180)
+        y_vel = -1 * self.speed * math.sin(theta * math.pi / 180) # remember down is positive for y
         return (x_vel, y_vel)
     
     def center_mass(self, visible_birds, bird_positions):
         '''
-        returns the center of mass of all of the visible birds
+        returns the center of mass of all of the VISIBLE birds
         if no birds are visible, continues to fly straight
         '''
         if not visible_birds:
@@ -120,7 +210,7 @@ class Boid(object):
         '''returns angle between (x1,y1) and (x2,y2)'''
         return math.atan2(-(y2 - y1), x2 - x1) * 180 / math.pi
     
-    def update(self, visible_birds, too_close_birds, bird_positions, bird_velocities, too_close_to_wall):
+    def update(self, visible_birds, too_close_birds, bird_positions, bird_velocities, too_close_to_wall, center_all_boids):
         '''
         Uses boid rules of separation, cohesion and alignment to update birds movement
         '''
@@ -136,32 +226,49 @@ class Boid(object):
             
             # fly towards the center of mass
             if self.behavior['center_of_mass']:
-                xbar, ybar = self.center_mass(visible_birds - too_close_to_wall, bird_positions)
-                theta_mass = self.angle(self.x, self.y, xbar, ybar)
+                #xbar, ybar = self.center_mass(visible_birds - too_close_to_wall, bird_positions)
+                xbar, ybar = center_all_boids
+                #xbar, ybar = self.center_mass(visible_birds, bird_positions)
+                theta_mass = self.angle(self.x, self.y, xbar, ybar) % 360
             
             # keep your distance from neighboring birds
             if self.behavior['social_distancing']:
                 if not too_close_birds:
-                    theta_social_dist = self.theta
+                    theta_social_dist = self.theta % 360
                 else:
-                    thetas = []
+                    behavior_thetas = []
                     for i in too_close_birds:
                         # +180 because we want to move away from visible birds
-                        thetas.append(self.angle(self.x, self.y, bird_positions[i][0], bird_positions[i][1]))
-                    theta_social_dist = 180 + sum(thetas) / len(thetas)
+                        behavior_thetas.append(self.angle(self.x, self.y, bird_positions[i][0], bird_positions[i][1]))
+                    theta_social_dist = (180 + sum(behavior_thetas) / len(behavior_thetas)) % 360
                 
             # align vector with neighboring birds
             if self.behavior['alignment']:
-                pass
+                if not visible_birds:
+                    theta_align = self.theta
+                else:
+                    theta_align = 0
+                    for i in visible_birds:
+                        theta_align += self.angle(0, 0, bird_velocities[i][0], bird_velocities[i][1])
+                    theta_align = theta_align // len(visible_birds)
         
             thetas = [theta_mass, theta_social_dist, theta_alignment]
-            #thetas = [self.angle(self.x, self.y, self.x_mid, self.y_mid)]*3
-        
+            #print()
+            #print([int(t) for t in thetas])
         
         
         weights = [self.behavior['center_of_mass'], self.behavior['social_distancing'], self.behavior['alignment']]
         target_theta = self.weighted_average(thetas, weights)
         target_theta %= 360
+        
+        # update birds position (consider doing this after updating direction)
+        x_vel, y_vel = self.get_vel()
+        self.x += x_vel
+        self.y += y_vel
+        self.x = max(self.x_min, min(self.x, self.x_max))
+        self.y = max(self.y_min, min(self.y, self.y_max))
+        
+        #print('curr/target',int(self.theta), int(target_theta))
         
         # update birds direction
         if abs(target_theta - self.theta) <= self.max_turn or abs(target_theta - 360 - self.theta) <= self.max_turn:
@@ -172,15 +279,9 @@ class Boid(object):
             else:
                 self.theta += self.max_turn if target_theta < self.theta else -self.max_turn
         self.theta %= 360
-            #t1, t2 = self.theta - self.max_turn, self.theta + self.max_turn
-            #self.theta = min(t1, t2, key = lambda t: abs(target_theta - t))
         
-        # update birds position
-        x_vel, y_vel = self.get_vel()
-        self.x += x_vel
-        self.y += y_vel
-        self.x = max(self.x_min, min(self.x, self.x_max))
-        self.y = max(self.y_min, min(self.y, self.y_max))
+        #print('new_theta',int(self.theta))
+        #print()
         
 
         
